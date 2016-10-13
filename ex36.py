83c602d84d7adccfbafe4c0bfbde925b6f5503cc
mermatriarchy <hailey.miller91@gmail.com>
2016-10-13 10:39:45 -0400

    add ex36 game

--- ex36.py
+++ ex36.py
@@ -9,11 +9,11 @@ def sleepy_cat():
 def destructive_cat():
 	print "You human has a plant perched on a window sill.\nDo you destroy it?"
 
-	choice = raw_imput("> ")
+	choice = raw_input("> ")
 	if ("yes" in choice) or ("destroy" in choice):
 		print "You knock over the plant and hear it crash onto the floor. You're satisfied with your destruction."
 		print "What do you do now? Are you tired or do you escape out the open window?"
-		choice = raw_imput("> ")
+		choice = raw_input("> ")
 
 		#This is how you decide what to do after you destory the plants
 		if "tired" in choice:
@@ -25,7 +25,34 @@ def destructive_cat():
 		print "You decide a nap sounds better, after all."
 		sleepy_cat()
 
-#This ends the program - might not need it eventually 
-def ending(why):
-	print why, "Good job." 
-	exit(0)
+#This happens when the cat goes out the window
+def adventurous_cat():
+	print "You jump out the window and make your way to the park down the street.\nYou see a bird perched above you in a tree."
+	print "Do you kill the bird or befriend it?"
+
+	choice = raw_input("> ")
+
+	if "kill" in choice:
+		print "You climb the tree and pounce on the bird."
+		print "You bring it home as a present for your owner. Good job!"
+		exit(0)
+	else:
+		print "You make friends with the bird and climb trees with it until you are tired, then return home."
+		sleepy_cat()
+
+def start():
+	print "Pretend you are a cat living with your humans in an apartment."
+	print "In morning you decide what to do with your day."
+	print "Do you want to take a nap or do you want to play?"
+
+	choice = raw_input("> ")
+
+	if "nap" in choice:
+		sleepy_cat()
+	elif "play" in choice:
+		destructive_cat()
+	else:
+		print "That's not something a cat would do. Try again."
+		start()
+
+start()
